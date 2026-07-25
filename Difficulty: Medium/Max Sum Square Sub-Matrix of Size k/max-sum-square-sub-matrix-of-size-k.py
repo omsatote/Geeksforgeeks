4f51fa8f3bfe0654bb8d48c959734ed6f4eca25e
Om Satote (について) <omsatote142005@gmail.com>
2026-07-25 19:04:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Sum Square Sub-Matrix of Size k/max-sum-square-sub-matrix-of-size-k.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maximumSum(self, mat, k):
+        n = len(mat)
+
+        # Prefix sum matrix
+        prefix = [[0] * (n + 1) for _ in range(n + 1)]
+
+        for i in range(n):
+            for j in range(n):
+                prefix[i + 1][j + 1] = (
+                    mat[i][j]
+                    + prefix[i][j + 1]
+                    + prefix[i + 1][j]
+                    - prefix[i][j]
+                )
+
+        ans = float('-inf')
+
+        for i in range(n - k + 1):
+            for j in range(n - k + 1):
+                total = (
+                    prefix[i + k][j + k]
+                    - prefix[i][j + k]
+                    - prefix[i + k][j]
+                    + prefix[i][j]
+                )
+                ans = max(ans, total)
+
+        return ans
