ceaa1a333835964de5377b1289552639c2906a8c
Om Satote (について) <omsatote142005@gmail.com>
2026-09-06 10:53:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of Pairwise ANDs/sum-of-pairwise-ands.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long pairAndSum(int[] Arr) {
+        long ans = 0;
+
+        for (int bit = 0; bit < 31; bit++) {
+            long count = 0;
+
+            for (int x : Arr) {
+                if ((x & (1 << bit)) != 0) {
+                    count++;
+                }
+            }
+
+            ans += count * (count - 1) / 2 * (1L << bit);
+        }
+
+        return ans;
+    }
+}
