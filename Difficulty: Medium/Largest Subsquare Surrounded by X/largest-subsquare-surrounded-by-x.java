8242536dd5725df400af59a454f3f1e3ecc52a88
Om Satote (について) <omsatote142005@gmail.com>
2026-09-20 17:30:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Largest Subsquare Surrounded by X/largest-subsquare-surrounded-by-x.java
@@ -0,0 +1,62 @@
+class Solution {
+
+    public int largestSubsquare(char[][] mat) {
+
+        int n = mat.length;
+
+        int[][] right = new int[n][n];
+        int[][] down = new int[n][n];
+
+        // Precompute consecutive X's to the right and downward
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+
+                if (mat[i][j] == 'X') {
+
+                    right[i][j] = 1;
+                    down[i][j] = 1;
+
+                    if (j + 1 < n) {
+                        right[i][j] += right[i][j + 1];
+                    }
+
+                    if (i + 1 < n) {
+                        down[i][j] += down[i + 1][j];
+                    }
+                }
+            }
+        }
+
+        int ans = 0;
+
+        // Check every cell as the top-left corner
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                int maxSize = Math.min(right[i][j], down[i][j]);
+
+                for (int size = maxSize; size > ans; size--) {
+
+                    int bottom = i + size - 1;
+                    int lastCol = j + size - 1;
+
+                    if (bottom < n && lastCol < n) {
+
+                        // Bottom boundary
+                        boolean bottomOK = right[bottom][j] >= size;
+
+                        // Right boundary
+                        boolean rightOK = down[i][lastCol] >= size;
+
+                        if (bottomOK && rightOK) {
+                            ans = size;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+}
